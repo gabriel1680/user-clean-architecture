9d39bc7fad2f90bac57301d70b307255cf7dd8c3
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2022-02-13 23:13:52 -0300

    refactor(domain/valueobjects): add email to index

--- src/domain/entities/user/valueobjects/index.ts
+++ src/domain/entities/user/valueobjects/index.ts
@@ -1,3 +1,4 @@
 export { default as  Password } from "./Password";
 export { default as  Name } from "./Name";
 export { default as Role } from "./Role";
+export { default as Email } from "./Email";
